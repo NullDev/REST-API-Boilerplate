d90f6f985236d317cad08005afdd29237000cd02
NullDev (Chris) <shadow@nulldev.org>
2019-03-14 21:36:31 +0100

    services

---
+++ src/services/portCheck.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+module.exports = function(port){
+    const appPort = port || 3000;
+
+    if (!port) log.warn("No port specified. Using default: 3000");
+
+    if (appPort < 1 || appPort > 65535){
+        log.error(`Invalid port specified: ${appPort}\nStopping...`);
+        process.exit(1);
+    }
+
+    else return appPort;
+};
---
+++ src/services/rootHandler.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+module.exports = function(req, res, app){
+    let respone = {
+        "status": "ok"
+    };
+
+    res.set({
+        "Content-Type": "application/json; charset=utf-8"
+    }).status(200).send(respone);
+};
