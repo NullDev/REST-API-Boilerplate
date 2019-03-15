f4d512dd3e91f6f263990507a3df820d9205c843
NullDev (Chris) <shadow@nulldev.org>
2019-03-15 19:47:51 +0100

    Fixed router

--- src/routes/routerV1.js
+++ src/routes/routerV1.js
@@ -7,37 +7,15 @@
 // Dependendies
 let express = require("express");
 
-// Core Modules
-let fs = require("fs");
-let path = require("path");
-
-// Utils
-let log = require("../utils/logger");
-let config = require("../utils/configHandler").getConfig();
-
-// Modules
-let getRoutes = require("./modules/getRoutes");
-let robotsHandler = require("./modules/robotsHandler");
-let sitemapHandler = require("./modules/sitemapHandler");
-
 // Init router
-let routes;
+// eslint-disable-next-line new-cap
 let router = express.Router();
 
 // Routes
 router.get("/", require("./v1/apiRoot"));
 
-router.get("/user/get", require("./v1/users/get"));
-router.get("/user/getAll", require("./v1/users/getAll"));
-
-// Modules
-router.get("/robots.txt", robotsHandler);
-router.get("/sitemap.xml", (req, res) => {
-    sitemapHandler(req, res, routes);
-});
-
-routes = getRoutes(router);
-log.info("Registered routes for V1: ");
-for (let i in routes) log.info("    Route '" + routes[i].path + "' registered with methods '" + (routes[i].methods).join(", ") + "'");
+router.get("/users/", require("./v1/users/users"));
+router.get("/users/get", require("./v1/users/get"));
+router.get("/users/getAll", require("./v1/users/getAll"));
 
 module.exports = router;
--- src/routes/routerV2.js
+++ src/routes/routerV2.js
@@ -7,37 +7,15 @@
 // Dependendies
 let express = require("express");
 
-// Core Modules
-let fs = require("fs");
-let path = require("path");
-
-// Utils
-let log = require("../utils/logger");
-let config = require("../utils/configHandler").getConfig();
-
-// Modules
-let getRoutes = require("./modules/getRoutes");
-let robotsHandler = require("./modules/robotsHandler");
-let sitemapHandler = require("./modules/sitemapHandler");
-
 // Init router
-let routes;
+// eslint-disable-next-line new-cap
 let router = express.Router();
 
 // Routes
 router.get("/", require("./v2/apiRoot"));
 
+router.get("/users/", require("./v2/users/users"));
 router.get("/users/get", require("./v2/users/get"));
 router.get("/users/getAll", require("./v2/users/getAll"));
-
-// Modules
-router.get("/robots.txt", robotsHandler);
-router.get("/sitemap.xml", (req, res) => {
-    sitemapHandler(req, res, routes);
-});
-
-routes = getRoutes(router);
-log.info("Registered routes for V2: ");
-for (let i in routes) log.info("    Route '" + routes[i].path + "' registered with methods '" + (routes[i].methods).join(", ") + "'");
 
 module.exports = router;
